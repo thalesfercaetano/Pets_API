3f8570156051cfeddd8f6c3d8dd83946754c26fc
Thales Caetano <thalesfercaetano@gmail.com>
2025-10-14 20:07:54 -0300

    pastas melhor organizadas

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const app = express();
-const PORT = process.env.PORT || 3003;
+export const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server rodando em:  http://localhost:${PORT}`);
+app.listen(3003, () => {
+ console.log("Servidor rodando na porta 3003! ");
 });
